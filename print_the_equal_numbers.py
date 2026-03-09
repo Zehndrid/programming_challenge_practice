163b9057a9e6a91b39040906ed582e1d7be352a4
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-09 22:29:45 +0800

    The program will show it the numbers are equal or not

---
+++ print_the_equal_numbers.py
@@ -0,0 +1,10 @@
+#input1
+first_num = int(input("Enter the first number:"))
+#input2
+second_num = int(input("Enter the second number:"))
+#decision part were the program will print equal if the numbers are the same
+
+if first_num == second_num:
+    print("Both number are equal")
+else:
+    print("Not Equal")
