fbd130afbde80f4031010f558aa1dc2156f1b7d5
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-09 22:17:37 +0800

    writing the code

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,5 +1,12 @@
 #input1
-
+first_num = int(input("Enter the first number:"))
 #input2
+second_num = int(input("Enter the second number:"))
+#decision part
 
-#
+if first_num > second_num:
+    print("The first number is bigger")
+elif second_num > first_num:
+    print("The second number is bigger")
+else:
+    print("Both numbers are equal")
