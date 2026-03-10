40eaa359ab5d38b708c65f7de8a98daab48f790e
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-10 23:55:29 +0800

    Created the program that gets the sum of two input numbers

---
+++ print_the_sum_of_two_numbers.py
@@ -0,0 +1,9 @@
+#batch1_no3
+#input1
+first_num = int(input("Enter the first number: "))
+#input2
+second_num = int(input("Enter the second number: "))
+
+#adding the two numbers
+total_sum = first_num + second_num
+print("The sum of two numbers is equal to:", total_sum)
