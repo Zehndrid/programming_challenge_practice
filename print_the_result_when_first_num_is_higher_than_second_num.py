20b5bd3bcbfc7bc7f0c36bf480d70ab9f43bf5ae
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 00:47:36 +0800

    Created the program for which will print the raised number of first num(base) with the second num(expon ent)

---
+++ print_the_result_when_first_num_is_higher_than_second_num.py
@@ -0,0 +1,8 @@
+#batch1_no6
+#enter the number for the base number and exponent number
+base_number = float(input("Enter the first number(base): "))
+exponent_number = float(input("Enter the second number(exponent): "))
+
+# Calculate the result using the ** operator
+total_power = base_number ** exponent_number
+print(base_number, "raised to the power of ", exponent_number, "is", total_power)
