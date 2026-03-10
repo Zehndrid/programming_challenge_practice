cd76bb9f07940708e2fb62ed456a328d99edb144
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 00:08:09 +0800

    Created the programm that will get the product of two numbers

---
+++ print_the_product_of_two_numbers.py
@@ -0,0 +1,9 @@
+#batch1_no4
+#input1
+first_num = int(input("Enter the first number: "))
+#input2
+second_num = int(input("Enter the second number: "))
+
+#getting the product of two numbers
+total_product = first_num * second_num
+print("The product of two numbers is equal to:", total_product )
