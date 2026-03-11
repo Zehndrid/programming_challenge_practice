827792ea4177c062bfe0013980b3dad8b51791b1
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 20:17:12 +0800

    Created a program that will print the difference of two number regarding of its sequence

---
+++ print_the_difference_of_two_numbers.py
@@ -0,0 +1,10 @@
+#batch2_no3
+#inputs
+first_num = float(input("Enter the first number: "))
+second_num = float(input("Enter the second number: "))
+
+#getting the difference of two numbers
+if first_num > second_num:
+    print("The difference of two numbers is: ", first_num - second_num)
+else:
+    print("The difference of the numbers is: ", second_num - first_num)
