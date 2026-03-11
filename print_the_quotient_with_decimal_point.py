6e3208a640fd8c11b68cb4038e42f40c9b8bb493
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 12:39:46 +0800

    Corrected the systax error function change it to math error when enter number 0 in second_num

--- print_the_quotient_with_decimal_point.py
+++ print_the_quotient_with_decimal_point.py
@@ -7,8 +7,8 @@ second_num = float(input("Enter the second number: "))
 #getting the quotient with decimal point
 total_quotient = first_num / second_num 
 
-if second_num > first_num:
-    print("Syntax error")
+if second_num == 0:
+    print("Math error")
 #if the numbers are entered in wrong sequence
 else:
     print(f"The total quotient is: {total_quotient:.2f}")
