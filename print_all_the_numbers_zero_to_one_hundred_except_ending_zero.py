dc81c5c122e80fd341aab0ffa62368521b3c46d4
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 19:48:16 +0800

    Created the program of printing the numbers from 0-100 and skipping ending with zero

---
+++ print_all_the_numbers_zero_to_one_hundred_except_ending_zero.py
@@ -0,0 +1,5 @@
+#batch1_no10
+#using for loop dividing by 10 and the result is not zero, the number does not end zero
+for current_number in range(101):
+    if current_number % 10 !=0:
+        print(current_number)
