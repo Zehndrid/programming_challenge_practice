61b8aa66db9ca3915ec10bab19ba37105ffad710
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-11 20:03:01 +0800

    Created the program which will prin not equal if the two numbers are not the same

---
+++ print_the_not_equal_when_the_numbers_are_not_the_same.py
@@ -0,0 +1,12 @@
+#batch2_no2
+#inputs
+first_num = int(input("Enter the first number:"))
+second_num = int(input("Enter the second number:"))
+
+#if the two numbers are not the same it will print not equal
+if first_num > second_num:
+    print("Not Equal")
+elif second_num > first_num:
+    print("Not Equal")
+else:
+    print("The numbers are equal")
