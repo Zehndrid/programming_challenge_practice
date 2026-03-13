19d38c76d2eb32eefbf9e20f9fe1db2bd37ecb22
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-13 19:15:19 +0800

    Created the program that will print all lower case

---
+++ print_the_input_in_all_lower_case.py
@@ -0,0 +1,6 @@
+#batch5_no4
+#input a string
+user_fullname = input("Enter your full name: ")
+#using lower() to convert the input to all lower case
+lower_fullname = user_fullname.lower()
+print(f"Output: {lower_fullname}")  
