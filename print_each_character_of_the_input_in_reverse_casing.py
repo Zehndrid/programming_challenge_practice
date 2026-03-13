a139cd35490ce11a86472d8d803d1985ab0afbd5
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-13 19:31:37 +0800

    Created a program that will reverse the casing of incorrect casing

---
+++ print_each_character_of_the_input_in_reverse_casing.py
@@ -0,0 +1,6 @@
+#batch5_no6
+#input a string
+user_fullname = input("Enter your full name: ")
+#using swapcase() to print the input in reverse casing
+reverse_casing_fullname = user_fullname.swapcase()
+print(f"Output: {reverse_casing_fullname}")
