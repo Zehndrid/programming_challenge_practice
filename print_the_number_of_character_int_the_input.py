bbf78d93102765ef2bb0b8aa42de06f08dbc0a0f
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-13 20:19:19 +0800

    Created the program that will print the character of the fullname of the user

---
+++ print_the_number_of_character_int_the_input.py
@@ -0,0 +1,6 @@
+#batch5_no8
+#input the full name of the user
+user_fullname = input("Enter your full name: ")
+#using len() to count the number of characters in the input
+number_of_characters = len(user_fullname)
+print(f"Output: The number of characters in the input is {number_of_characters}.")
