9dd84681665ab08d1d2687b940c059adbb4f2b75
Zehndrid <150003385+Zehndrid@users.noreply.github.com>
2026-03-13 19:07:35 +0800

    Created the program that will convert small_letters into capital letters

---
+++ print_a_string_that_will_convert_small_letters_to_capital_letters.py
@@ -0,0 +1,7 @@
+#batch5_no3
+#input a string with small letters
+user_fullname = input("Enter your full name: ")
+
+#using upper() to convert small letters to capital letters
+capital_fullname = user_fullname.upper()
+print(f"Output: {capital_fullname}")
